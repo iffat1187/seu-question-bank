508d920aa5dd6febd401c0d10fcc6f4c17266322
iffatjahan1187 <iffatjahan1470@gmail.com>
2026-09-23 12:07:19 +0600

    initial set up complete

---
+++ src/main/java/com/seu/seuquestionbank/SeuQuestionBankApplication.java
@@ -0,0 +1,13 @@
+package com.seu.seuquestionbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeuQuestionBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeuQuestionBankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/seu/seuquestionbank/SeuQuestionBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seu.seuquestionbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeuQuestionBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
